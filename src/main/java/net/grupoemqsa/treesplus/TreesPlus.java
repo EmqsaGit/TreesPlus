fd8aff58e5a247b450337a8418fe78c136b31e20
EmqsaGit <eduardotenorioposec@gmail.com>
2022-09-20 11:35:58 -0300

    1st(?) commit

---
+++ src/main/java/net/grupoemqsa/treesplus/TreesPlus.java
@@ -0,0 +1,15 @@
+package net.grupoemqsa.treesplus;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TreesPlus implements ModInitializer {
+	public static final String TREESPLUS = "treesplus";
+	public static final Logger LOGGER = LoggerFactory.getLogger(TREESPLUS);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
